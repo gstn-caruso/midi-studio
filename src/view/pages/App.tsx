7eacc715226ebe6891c53ef3dd2421fd7c0036f7
Gastón Caruso <gstn.caruso@gmail.com>
2022-12-09 15:19:24 -0300

    refactor project structure + installs styled components

---
+++ src/view/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components";
+
+export function App() {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+
+const Container = styled.div`
+`
